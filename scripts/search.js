d62e9412df757e07dd4ed39070b5a2ae6f5e95c1
rhaddad7 <rhaddad7@gatech.edu>
2018-08-22 16:13:40 -0400

    Search Functions

--- scripts/search.js
+++ scripts/search.js
@@ -22,3 +22,40 @@ function closeSearch(){
 	$('.search-modal-c').css('opacity', '0');
 	$('.search-modal-c').css('transform', 'translate3d(0px, -15px, 0px)');
 }
+
+/**
+ * Search a SoundCloud Username.
+ * @param {String} - Search Term (Username)
+ */
+function searchInput(searchTerm){
+	var proxy = 'https://cors-anywhere.herokuapp.com/';
+	var url = 'https://api-v2.soundcloud.com/search/users?q=' + searchTerm + '&sc_a_id=1331a031-e3c8-415e-894d-f1458fcbe40a&variant_ids=1053&facet=place&user_id=851332-319840-3643-205054&client_id=lj8dF0xi5RcKDD1dLa6IOY6orLH3uBJW&limit=10&offset=0&linked_partitioning=1&app_version=1527169554&app_locale=en';
+	var fullurl = proxy + url;
+	fetch(fullurl)
+	  .then((response) => {
+		return response.json();
+	  })
+	  .then((searchResults) => {
+		console.log(searchResults);
+	  })
+	  .catch((error) => {
+		console.log(error);
+	  });
+}
+
+/**
+ * Load the search results
+ * @param {None}
+ */
+function loadSearch(){
+
+}
+
+/**
+ *  Load a clicked user to DOM
+ *  @param {None}
+ */
+function searchEntryClicked(){
+
+
+}

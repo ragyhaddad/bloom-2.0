de25f5e7b6f7d1f5d67be9ee32f4cfbfc0907ea4
Seb <seb.o.wetzel@gmail.com>
2018-08-26 18:09:50 -0400

    Weekly css

--- scripts/weekly.js
+++ scripts/weekly.js
@@ -1,26 +1,45 @@
-
 /**
  * Load 5 featured artists
  */
 function loadFeaturedArtists(){
     html = '';
-    for(x in favorites){
-        fetch(`https://api.soundcloud.com/users/${favorites[x]}?client_id=3Goi9X5NOF7g1ofGbmYEkpveejwvlqjd`)
+    g_weekly.featured_artists.forEach(artist => {
+        fetch(`https://api.soundcloud.com/users/${artist}?client_id=3Goi9X5NOF7g1ofGbmYEkpveejwvlqjd`)
         .then((response) => {
             return response.json();
         })
         .then((data)=>{
-            html += `<li onclick="searchEntryClicked('${data.permalink}')" class="search-item">` +
-					`<img src="${data.avatar_url}" draggable="false">` +
-					`<div class="search-user-info">` +
-						`<h3>${data.username}</h3>` +
-						`<p>${parseCount(data.followers_count)}</p>` +
-					`</div>` +
-                `</li>`;
-        $('.featured-list')[0].innerHTML = html;       
-        })
-    }  
-    
+            $('.featured-artists')[0].innerHTML += `<li onclick="fetchUser('${data.permalink}')">` +
+			    `<img src="${data.avatar_url}" draggable="false">` +
+				`<div class="featured-info">` +
+					`<h3>${data.username}</h3>` +
+					`<p>${parseCount(data.followers_count)}</p>` +
+				`</div>` +
+            `</li>`;     
+        });
+    }); 
 }
+loadFeaturedArtists();
 
-loadFeaturedArtists()
+/**
+ * Load 5 featured artists
+ */
+function loadFeaturedLabels(){
+    html = '';
+    g_weekly.featured_labels.forEach(label => {
+        fetch(`https://api.soundcloud.com/users/${label}?client_id=3Goi9X5NOF7g1ofGbmYEkpveejwvlqjd`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data)=>{
+            $('.featured-labels')[0].innerHTML += `<li onclick="fetchUser('${data.permalink}')">` +
+			    `<img src="${data.avatar_url}" draggable="false">` +
+				`<div class="featured-info">` +
+					`<h3>${data.username}</h3>` +
+					`<p>${parseCount(data.followers_count)}</p>` +
+				`</div>` +
+            `</li>`;     
+        });
+    }); 
+}
+loadFeaturedLabels();
